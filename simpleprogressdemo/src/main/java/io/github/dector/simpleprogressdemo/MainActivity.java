e6e24f251428b7c503234a3235d1b41905fe2418
Denys M <dector9@gmail.com>
2015-02-05 15:02:33 +0200

    Added moar controls to demo

--- simpleprogressdemo/src/main/java/io/github/dector/simpleprogressdemo/MainActivity.java
+++ simpleprogressdemo/src/main/java/io/github/dector/simpleprogressdemo/MainActivity.java
@@ -5,14 +5,17 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-
-import java.util.concurrent.TimeUnit;
+import android.widget.TextView;
 
 import io.github.dector.simpleprogress.ProgressView;
 
 public class MainActivity extends ActionBarActivity {
 
     private Button startButton;
+    private Button resetButton;
+    private Button plusButton;
+    private Button minusButton;
+    private TextView progressValue;
     private ProgressView progress;
 
     @Override
@@ -21,6 +24,10 @@ public class MainActivity extends ActionBarActivity {
         setContentView(R.layout.activity_main);
 
         startButton = (Button) findViewById(R.id.start_button);
+        resetButton = (Button) findViewById(R.id.reset_button);
+        plusButton = (Button) findViewById(R.id.plus_button);
+        minusButton = (Button) findViewById(R.id.minus_button);
+        progressValue = (TextView) findViewById(R.id.value);
         progress = (ProgressView) findViewById(R.id.progress);
 
         startButton.setOnClickListener(new View.OnClickListener() {
@@ -29,6 +36,38 @@ public class MainActivity extends ActionBarActivity {
                 new ProgressAsyncTask().execute();
             }
         });
+        resetButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                updateProgress(0);
+            }
+        });
+        plusButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                updateProgress(progress.getValue()+1);
+            }
+        });
+        minusButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                updateProgress(progress.getValue()-1);
+            }
+        });
+
+        updateProgress(progress.getValue());
+    }
+
+    private void updateProgress(int value) {
+        progress.setValue(value);
+        progressValue.setText(Integer.toString(progress.getValue()));
+    }
+
+    private void setControlsEnabled(boolean enabled) {
+        startButton.setEnabled(enabled);
+        resetButton.setEnabled(enabled);
+        plusButton.setEnabled(enabled);
+        minusButton.setEnabled(enabled);
     }
 
     private class ProgressAsyncTask extends AsyncTask<Void, Integer, Void> {
@@ -51,19 +90,20 @@ public class MainActivity extends ActionBarActivity {
         @Override
         protected void onProgressUpdate(Integer... values) {
             super.onProgressUpdate(values);
-            progress.setValue(values[0]);
+            updateProgress(values[0]);
         }
 
         @Override
         protected void onPreExecute() {
             super.onPreExecute();
-            startButton.setEnabled(false);
+            setControlsEnabled(false);
         }
 
         @Override
         protected void onPostExecute(Void aVoid) {
             super.onPostExecute(aVoid);
-            startButton.setEnabled(true);
+            updateProgress(100);
+            setControlsEnabled(true);
         }
     }
 }
